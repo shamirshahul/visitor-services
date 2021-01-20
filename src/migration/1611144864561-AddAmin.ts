d3827d116a407ae939f659a2768d1b2fa88bb67c
shamirshahul <shamir@nuevezo.com>
2021-01-20 17:11:44 +0400

    silly mistake

--- src/migration/1611144864561-AddAmin.ts
+++ src/migration/1611144864561-AddAmin.ts
@@ -4,13 +4,11 @@ import * as bcrypt from "bcrypt";
 export class AddAmin1611144864561 implements MigrationInterface {
   name = "AddAmin1611144864561";
   public async up(queryRunner: QueryRunner): Promise<void> {
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash("admin@123", salt, async (_err, hash) => {
-        await queryRunner.query(
-          `INSERT INTO public."admin" ("firstName", "lastName", username, phonenumber, "password") VALUES('admin', 'admin', 'admin', '1234567890', '${hash}');`
-        );
-      });
-    });
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash("admin@123", salt);
+    await queryRunner.query(
+      `INSERT INTO public."admin" ("firstName", "lastName", username, phonenumber, "password") VALUES('admin', 'admin', 'admin', '1234567890', '${hash}');`
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
