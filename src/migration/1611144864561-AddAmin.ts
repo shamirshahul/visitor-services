faa24cb5d5ff2f16abe9b582c98b82643a6c9082
rishabhnuevezo <rishabh@nuevezo.com>
2021-01-20 16:47:55 +0400

    Temp admin creation migration

--- src/migration/1611144864561-AddAmin.ts
+++ src/migration/1611144864561-AddAmin.ts
@@ -1,11 +1,16 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
+import * as bcrypt from "bcrypt";
 
 export class AddAmin1611144864561 implements MigrationInterface {
   name = "AddAmin1611144864561";
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `INSERT INTO public."admin" ("firstName", "lastName", username, phonenumber, "password") VALUES('admin', 'admin', 'admin', '1234567890', 'admin@123');`
-    );
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash("admin@123", salt, async (_err, hash) => {
+        await queryRunner.query(
+          `INSERT INTO public."admin" ("firstName", "lastName", username, phonenumber, "password") VALUES('admin', 'admin', 'admin', '1234567890', '${hash}');`
+        );
+      });
+    });
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
