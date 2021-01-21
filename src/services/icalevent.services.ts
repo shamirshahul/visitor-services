ce8a218c9e71f696ed0ab9cb905dc92d5ec6949a
shamirshahul <shamir@nuevezo.com>
2021-01-21 10:40:34 +0400

    Starting integration of Ical

--- src/services/icalevent.services.ts
+++ src/services/icalevent.services.ts
@@ -3,7 +3,22 @@ import { Application } from "express";
 import { Connection } from "typeorm";
 import * as nodemailer from "nodemailer";
 import * as fs from "fs";
+import ical from "ical-generator";
+import moment from "moment";
 
 module.exports = (app: Application, connection: Connection) => {
-  console.log("ofweuig");
+  app.post("/calendar", (req, res) => {
+    const cal = ical({ domain: "nuevezo.com", name: "my first iCal" });
+    cal.save(`${__dirname}/cals/cal.ical`, () => {
+      res.send("calendar created");
+    });
+    // cal.createEvent({
+    //   start: moment(),
+    //   end: moment().add(1, "hour"),
+    //   summary: "Example Event",
+    //   description: "It works ;)",
+    //   location: "my room",
+    //   url: "https://nuevezo.com/",
+    // });
+  });
 };
