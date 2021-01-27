97c0ea92ea10913185eee0e955c0f074b7b8999c
shamirshahul <shamir@nuevezo.com>
2021-01-27 17:13:51 +0400

    no of attendees

--- src/services/Room.services.ts
+++ src/services/Room.services.ts
@@ -8,11 +8,11 @@ import { EquipmentType } from "../entity/EquipmnetType";
 module.exports = (app: Application, connection: Connection) => {
   app.post("/room/create", (req, res) => {
     req.body.equipments = req.body.equipments.map((equipment: Equipment) => {
-      equipment.equipmentType = <EquipmentType>equipment.equipmentType;
+      equipment.equipmentType = (equipment.equipmentType as EquipmentType);
       console.log(typeof equipment.equipmentType);
       return equipment;
     });
-    const rooms: Room = <Room>_.cloneDeep(req.body);
+    const rooms: Room = _.cloneDeep(req.body) as Room;
     console.log(rooms);
     connection.manager.save(rooms);
     res.type("json").status(201).send(rooms);
